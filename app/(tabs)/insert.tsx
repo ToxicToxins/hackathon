48beb7d4138a161afc80ea28c69c98363c286a39
Joey <Joeytran71@gmail.com>
2024-10-12 01:48:29 -0400

    make insert select from gallery

--- app/(tabs)/insert.tsx
+++ app/(tabs)/insert.tsx
@@ -1,5 +1,7 @@
 import { Text, View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { Image } from 'expo-image';
+import { useState } from 'react';
 
 import Button from '@/components/Buttons';
 import ImageViewer from "@/components/ImageViewer";
@@ -7,13 +9,30 @@ import ImageViewer from "@/components/ImageViewer";
 const PlaceholderImage = require('../../assets/images/background-image.jpg');
 
 export default function InsertScreen() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
         <Button label="Use this photo" />
       </View>
     </View>

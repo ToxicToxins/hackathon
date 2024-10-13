f0a73a80677bd46f9c6037308edd41c304145aff
Aaron Park <135677875+aaronmpark@users.noreply.github.com>
2024-10-13 03:53:51 -0400

    random weird

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,18 +14,3 @@ export default function RootLayout() {
     </Stack>
   );
 }
-=======
-    
-    <Stack screenOptions={{
-      headerStyle: {
-        backgroundColor: '#25292e', // Match this color with your tab bar
-      },
-      headerTintColor: '#fff',
-    }}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-  );
-  
-}
->>>>>>> master

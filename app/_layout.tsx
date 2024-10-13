d9e786c0e9e0410da342d7e5bbf2ee4067f323b0
kyle <50057148+Kylesonzy@users.noreply.github.com>
2024-10-13 03:00:42 -0400

    appcommits

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,16 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    
+    <Stack screenOptions={{
+      headerStyle: {
+        backgroundColor: '#25292e', // Match this color with your tab bar
+      },
+      headerTintColor: '#fff',
+    }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
+  
 }

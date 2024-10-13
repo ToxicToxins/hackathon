33f22144f9072e228c013c971f2bfd90ce50bbaf
Joey <Joeytran71@gmail.com>
2024-10-12 22:46:44 -0400

    add testing links

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import { Link } from 'expo-router';
-import { red } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+import Button from '@/components/Buttons';
 
 export default function Page() {
   return (
     <View style={styles.container}>
+      <ScrollView>
       <Text style={styles.titleText}>Welcome! </Text>
       <Text style={styles.bodyText}>This app takes a picture of your face,
       scans it for acne, and recommends some products to help treat it.</Text>
       <Text style={styles.bodyText2}>Please fill out some questions to make it easier for the app to recommend you products.</Text>
-
-      {/* <Link href="/loading" style={styles.button}>Loading</Link>
-      <Link href="/output" style={styles.button}>Output</Link> */}
+      <Link href="/loading" style={styles.button}>Loading</Link>
+      <Link href="/output" style={styles.button}>Output</Link>
+      <Link href="/insert" style={styles.button}>Insert</Link>
+      </ScrollView>
     </View>
   );
 }
@@ -22,11 +24,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    paddingHorizontal: 16
   },
   titleText: {
     color: '#fff',
     fontSize: 30,
     marginTop: 20,
+    textAlign: 'center'
   },
   bodyText: {
     color: '#fff',
@@ -42,7 +46,10 @@ const styles = StyleSheet.create({
   },
   button: {
     fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    color: '#ffffff',
+    textAlign: 'center'
   },
 });

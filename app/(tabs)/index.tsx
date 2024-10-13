fcdd60b34e1b358de986d7aa4274b5e9c0287b7e
mrterror2 <dmitrykimchi@gmail.com>
2024-10-12 20:30:59 -0400

    Added welcome text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
+import { red } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <Link href="/loading" style={styles.button}>Loading</Link>
-      <Link href="/output" style={styles.button}>Output</Link>
-      <Link href="/qna" style={styles.button}>QnA</Link>
+      <Text style={styles.titleText}>Welcome! </Text>
+      <Text style={styles.bodyText}>This app takes a picture of your face,
+      scans it for acne, and recommends some products to help treat it.</Text>
+      <Text style={styles.bodyText2}>Please fill out some questions to make it easier for the app to recommend you products.</Text>
+
+      {/* <Link href="/loading" style={styles.button}>Loading</Link>
+      <Link href="/output" style={styles.button}>Output</Link> */}
     </View>
   );
 }
@@ -16,10 +21,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
-  text: {
+  titleText: {
     color: '#fff',
+    fontSize: 30,
+    marginTop: 20,
+  },
+  bodyText: {
+    color: '#fff',
+    fontSize: 18,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  bodyText2: {
+    color: '#fff',
+    fontSize: 18,
+    marginTop: 175,
+    textAlign: 'center',
   },
   button: {
     fontSize: 20,
